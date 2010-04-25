6af2144cb167f077c9cded521a0338cbe269dbcf
andrea.fazzi@alcacoop.it <andrea.fazzi@alcacoop.it>
2010-04-25 17:15:37 +0200

    First pages commit

---
+++ gospecify-setup/spec/fib_spec.go
@@ -0,0 +1,20 @@
+package main
+
+import . "specify"
+import t "../src/fib"
+
+func init() {
+	Describe("Fib", func() {
+		It("should return Fibonacci's next number", func(e Example) {
+			fib := new(t.Fib)
+			fib.N0, fib.N1 = 0, 1
+			e.Value(fib.Next()).Should(Be(1))
+			e.Value(fib.Next()).Should(Be(1))
+			e.Value(fib.Next()).Should(Be(2))
+			e.Value(fib.Next()).Should(Be(3))
+			e.Value(fib.Next()).Should(Be(5))
+		})
+
+	})
+}
+
